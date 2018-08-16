239c6a192165b58109ab84c75a885aa27da948cc
charli <carlostoro04@gmail.com>
2018-08-16 17:38:06 -0500

    configuracion de archivos estaticos

--- refugio/apps/mascota/views.py
+++ refugio/apps/mascota/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from apps.mascota.forms import MascotaForm
 
 
 def index(request):
-    return HttpResponse('Index')
+    return render(request, 'mascota/index.html')
+
+
+def mascota_view(request):
+    if request.method == 'POST':
+        form = MascotaForm(request.POST)
+        if form.is_valid():
+            form.save()

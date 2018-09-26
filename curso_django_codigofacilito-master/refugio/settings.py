c3aa64453c9153c00b86552dd8c779a2f24a721e
charli <carlostoro04@gmail.com>
2018-09-26 14:39:06 -0500

    primer commit de prueba

--- curso_django_codigofacilito-master/refugio/settings.py
+++ curso_django_codigofacilito-master/refugio/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.9/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
-
+'''asjlasjdaklsd'''
 import os
 from django.core.urlresolvers import reverse_lazy
 
